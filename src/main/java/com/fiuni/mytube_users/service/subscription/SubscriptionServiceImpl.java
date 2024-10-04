2811c84d31f9b61b3b0220e773e578b98fa81bee
jorgef29 <jorgefigueredo2001@gmail.com>
2024-10-04 14:04:35 -0400

    ejemplo para never/required y llamada indirecta de not_supported y never

--- src/main/java/com/fiuni/mytube_users/service/subscription/SubscriptionServiceImpl.java
+++ src/main/java/com/fiuni/mytube_users/service/subscription/SubscriptionServiceImpl.java
@@ -1,157 +1,191 @@
-package com.fiuni.mytube_users.service.subscription;
+    package com.fiuni.mytube_users.service.subscription;
 
-import com.fiuni.mytube.domain.channel.ChannelDomain;
-import com.fiuni.mytube.domain.subscription.SubscriptionDomain;
-import com.fiuni.mytube.domain.user.UserDomain;
-import com.fiuni.mytube.dto.subscription.SubscriptionDTO;
-import com.fiuni.mytube.dto.subscription.SubscriptionResult;
-import com.fiuni.mytube_users.dao.ISubscriptionDao;
-import com.fiuni.mytube_users.dao.IUserDao;
-import com.fiuni.mytube_users.exception.BadRequestException;
-import com.fiuni.mytube_users.exception.ResourceNotFoundException;
-import com.fiuni.mytube_users.service.baseService.BaseServiceImpl;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import com.fiuni.mytube_users.exception.ResourceNotFoundException;
+    import com.fiuni.mytube.domain.channel.ChannelDomain;
+    import com.fiuni.mytube.domain.subscription.SubscriptionDomain;
+    import com.fiuni.mytube.domain.user.UserDomain;
+    import com.fiuni.mytube.dto.subscription.SubscriptionDTO;
+    import com.fiuni.mytube.dto.subscription.SubscriptionResult;
+    import com.fiuni.mytube_users.dao.ISubscriptionDao;
+    import com.fiuni.mytube_users.dao.IUserDao;
+    import com.fiuni.mytube_users.exception.BadRequestException;
+    import com.fiuni.mytube_users.exception.ResourceNotFoundException;
+    import com.fiuni.mytube_users.service.baseService.BaseServiceImpl;
+    import lombok.extern.slf4j.Slf4j;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.cache.annotation.CacheEvict;
+    import org.springframework.cache.annotation.CachePut;
+    import org.springframework.cache.annotation.Cacheable;
+    import org.springframework.data.domain.Page;
+    import org.springframework.data.domain.Pageable;
+    import org.springframework.data.redis.cache.RedisCacheManager;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.stereotype.Service;
+    import org.springframework.transaction.annotation.Propagation;
+    import org.springframework.transaction.annotation.Transactional;
+    import org.springframework.web.server.ResponseStatusException;
+    import com.fiuni.mytube_users.exception.ResourceNotFoundException;
 
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
+    import java.util.Date;
+    import java.util.List;
+    import java.util.stream.Collectors;
 
-@Service
-@Slf4j
-public class SubscriptionServiceImpl extends BaseServiceImpl<SubscriptionDTO, SubscriptionDomain, SubscriptionResult> implements ISubscriptionService {
-    @Autowired
-    private IUserDao userDao;
-    @Autowired
-    private ISubscriptionDao subscriptionDao;
-    @Autowired
-    private RedisCacheManager redisCacheManager;
+    @Service
+    @Slf4j
+    public class SubscriptionServiceImpl extends BaseServiceImpl<SubscriptionDTO, SubscriptionDomain, SubscriptionResult> implements ISubscriptionService {
+        @Autowired
+        private IUserDao userDao;
+        @Autowired
+        private ISubscriptionDao subscriptionDao;
+        @Autowired
+        private RedisCacheManager redisCacheManager;
 
-    @Override
-    protected SubscriptionDTO convertDomainToDto(SubscriptionDomain domain) {
-        // Convertir SubscriptionDomain a SubscriptionDTO
-        SubscriptionDTO dto = new SubscriptionDTO();
-        dto.set_id(domain.getId());
-        dto.setUserId(domain.getUser().getId());
-        dto.setChannelId(1);
-        dto.setSubscriptionDate(domain.getSubscriptionDate());
-        return dto;
-    }
-
-    @Override
-    protected SubscriptionDomain convertDtoToDomain(SubscriptionDTO dto) {
-        // Convertir SubscriptionDTO a SubscriptionDomain
-        SubscriptionDomain domain = new SubscriptionDomain();
-
-        // Buscar el usuario por ID
-        UserDomain user = userDao.findById(dto.getUserId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado"));
-
-        // Hardcodear el canal
-        ChannelDomain channel = new ChannelDomain();
-        channel.setId(1); // ID del canal hardcodeado
-        channel.setChannelName("Canal predeterminado"); // Nombre del canal hardcodeado
-
-        // Asignar los valores a la suscripción
-        domain.setUser(user);
-        domain.setChannel(channel);
-        domain.setSubscriptionDate(dto.getSubscriptionDate() != null ? dto.getSubscriptionDate() : new Date());
-
-        return domain;
-    }
-
-    @Override
-    @CachePut(value = "my_tube_subscriptions",key = "'subscription_'+ #result.get_id()")
-    public SubscriptionDTO save(SubscriptionDTO dto) {
-        try{
-            SubscriptionDomain domain = convertDtoToDomain(dto);
-
-            // Guardar la suscripción en la base de datos
-            SubscriptionDomain savedDomain = subscriptionDao.save(domain);
-            log.info("subscription guardado en cache: {}", savedDomain);
-            // Convertir el dominio guardado de nuevo a DTO y devolverlo
-            return convertDomainToDto(savedDomain);
-        } catch (Exception e) {
-            throw new BadRequestException("Bad request to save subscription");
-        }
-    }
-
-    @Override
-    @Cacheable(value = "my_tube_subscription",key = "'subsctiption'+#id")
-    public SubscriptionDTO getById(Integer id) {
-        SubscriptionDomain domain = subscriptionDao.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Subscription con id: "+ id +" no encontrado"));
-
-        log.info("subscription guardado en cache: {}", domain);
-        return convertDomainToDto(domain);
-    }
-
-    @Override
-    public SubscriptionResult getAll() {
-        List<SubscriptionDomain> domains = subscriptionDao.findAll();
-
-        // Crear una lista de DTOs a partir de los dominios
-        List<SubscriptionDTO> dtos = domains.stream()
-                .map(this::convertDomainToDto)
-                .collect(Collectors.toList());
-
-        // Crear el resultado con la lista de DTOs
-        SubscriptionResult result = new SubscriptionResult();
-        result.setSubscriptions(dtos);
-        return result;
-    }
-
-    @Override
-    public List<SubscriptionDTO> getUserSubscriptions(Integer userId) {
-        // Obtener las suscripciones del usuario
-        List<SubscriptionDomain> subscriptions = subscriptionDao.findByUser_Id(userId);
-
-        // Convertir las suscripciones a DTOs
-        List<SubscriptionDTO> subscriptionDTOs = subscriptions.stream()
-                .map(this::convertDomainToDto)
-                .collect(Collectors.toList());
-
-        // Iterar sobre cada DTO y guardarlo en el caché
-        for (SubscriptionDTO dto : subscriptionDTOs) {
-            String cacheKey = "my_tube_subscriptions_user_" + dto.get_id(); // Crear la clave para cada suscripción
-            redisCacheManager.getCache("my_tube_subscriptions_user").put(cacheKey, dto);
-            log.info("Suscripción guardada en cache: {}", dto);
+        @Override
+        protected SubscriptionDTO convertDomainToDto(SubscriptionDomain domain) {
+            // Convertir SubscriptionDomain a SubscriptionDTO
+            SubscriptionDTO dto = new SubscriptionDTO();
+            dto.set_id(domain.getId());
+            dto.setUserId(domain.getUser().getId());
+            dto.setChannelId(1);
+            dto.setSubscriptionDate(domain.getSubscriptionDate());
+            return dto;
         }
 
-        return subscriptionDTOs;
-    }
+        @Override
+        protected SubscriptionDomain convertDtoToDomain(SubscriptionDTO dto) {
+            // Convertir SubscriptionDTO a SubscriptionDomain
+            SubscriptionDomain domain = new SubscriptionDomain();
 
-    @Override
-    @CacheEvict(value = "my_tube_subscriptions", key = "'subscription_'+#id")
-    public void deleteSubscription(Integer id) {
-        // Verificar si la suscripción existe
-        SubscriptionDomain subscription = subscriptionDao.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Subscription con id: "+ id +" no encontrado"));
+            // Buscar el usuario por ID
+            UserDomain user = userDao.findById(dto.getUserId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado"));
 
-        // Eliminar la suscripción
-        subscriptionDao.deleteById(id);
-    }
+            // Hardcodear el canal
+            ChannelDomain channel = new ChannelDomain();
+            channel.setId(1); // ID del canal harcodeado
+            channel.setChannelName("Canal predeterminado"); // Nombre del canal hardcodeado
 
-    @Override
-    public SubscriptionResult getAll(Pageable pageable) {
-        Page<SubscriptionDomain> page = subscriptionDao.findAll(pageable);
-        SubscriptionResult result = new SubscriptionResult();
-        List<SubscriptionDTO> dtos = convertDomainListToDtoList(page.getContent());
-        for(SubscriptionDTO dto : dtos) {
-            redisCacheManager.getCache("my_tube_subscriptions").put("my_tube_subscriptions" + dto.get_id(), dto);
-            log.info("en cache" + dto);
+            // Asignar los valores a la suscripción
+            domain.setUser(user);
+            domain.setChannel(channel);
+            //domain.setSubscriptionDate(dto.getSubscriptionDate() != null ? dto.getSubscriptionDate() : new Date());
+            domain.setSubscriptionDate(dto.getSubscriptionDate());
+
+            return domain;
         }
-        result.setSubscriptions(dtos);
-        return result;
+
+        @Override
+        @CachePut(value = "my_tube_subscriptions",key = "'subscription_'+ #result.get_id()")
+        @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+        public SubscriptionDTO save(SubscriptionDTO dto) {
+            try{
+                SubscriptionDomain domain = convertDtoToDomain(dto);
+
+                // Guardar la suscripción en la base de datos
+                SubscriptionDomain savedDomain = subscriptionDao.save(domain);
+                log.info("subscription guardado en cache: {}", savedDomain);
+                log.warn("Iniciando transaccion not_supported...");
+                /*try {
+                    setDateToday(savedDomain);
+                } catch (RuntimeException e) {
+                    log.error("Error en set date ", e);
+                }*/
+
+                setDateToday(savedDomain);
+                // Convertir el dominio guardado de nuevo a DTO y devolverlo
+                SubscriptionDTO result = convertDomainToDto(savedDomain);
+                log.warn("resultado: {}", result);
+                return result;
+            } catch (Exception e) {
+                throw new BadRequestException("Bad request to save subscription");
+            }
+        }
+
+        @Override
+        @Cacheable(value = "my_tube_subscription",key = "'subsctiption'+#id")
+        public SubscriptionDTO getById(Integer id) {
+            SubscriptionDomain domain = subscriptionDao.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Subscription con id: "+ id +" no encontrado"));
+
+            log.info("subscription guardado en cache: {}", domain);
+            return convertDomainToDto(domain);
+        }
+
+        @Override
+        public SubscriptionResult getAll() {
+            List<SubscriptionDomain> domains = subscriptionDao.findAll();
+
+            // Crear una lista de DTOs a partir de los dominios
+            List<SubscriptionDTO> dtos = domains.stream()
+                    .map(this::convertDomainToDto)
+                    .collect(Collectors.toList());
+
+            // Crear el resultado con la lista de DTOs
+            SubscriptionResult result = new SubscriptionResult();
+            result.setSubscriptions(dtos);
+            return result;
+        }
+
+        @Override
+        public List<SubscriptionDTO> getUserSubscriptions(Integer userId) {
+            // Obtener las suscripciones del usuario
+            List<SubscriptionDomain> subscriptions = subscriptionDao.findByUser_Id(userId);
+
+            // Convertir las suscripciones a DTOs
+            List<SubscriptionDTO> subscriptionDTOs = subscriptions.stream()
+                    .map(this::convertDomainToDto)
+                    .collect(Collectors.toList());
+
+            // Iterar sobre cada DTO y guardarlo en el caché
+            for (SubscriptionDTO dto : subscriptionDTOs) {
+                String cacheKey = "my_tube_subscriptions_user_" + dto.get_id(); // Crear la clave para cada suscripción
+                redisCacheManager.getCache("my_tube_subscriptions_user").put(cacheKey, dto);
+                log.info("Suscripción guardada en cache: {}", dto);
+            }
+
+            return subscriptionDTOs;
+        }
+
+        @Override
+        @CacheEvict(value = "my_tube_subscriptions", key = "'subscription_'+#id")
+        public void deleteSubscription(Integer id) {
+            // Verificar si la suscripción existe
+            SubscriptionDomain subscription = subscriptionDao.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Subscription con id: "+ id +" no encontrado"));
+
+            // Eliminar la suscripción
+            subscriptionDao.deleteById(id);
+        }
+
+        @Override
+        public SubscriptionResult getAll(Pageable pageable) {
+            Page<SubscriptionDomain> page = subscriptionDao.findAll(pageable);
+            SubscriptionResult result = new SubscriptionResult();
+            List<SubscriptionDTO> dtos = convertDomainListToDtoList(page.getContent());
+            for(SubscriptionDTO dto : dtos) {
+                redisCacheManager.getCache("my_tube_subscriptions").put("my_tube_subscriptions" + dto.get_id(), dto);
+                log.info("en cache" + dto);
+            }
+            result.setSubscriptions(dtos);
+            return result;
+        }
+
+        //FUNCIONES PARA LLAMADAS INDIRECTAS
+        @Transactional(propagation = Propagation.NEVER) // Fuera de la transacción principal
+        public void setDateToday(SubscriptionDomain subscription) {
+            log.warn("parametro: "+subscription.getSubscriptionDate());
+            try {
+                // Modificar la fecha de suscripción
+                subscription.setSubscriptionDate(new Date());
+
+                // Simular un fallo cuando el usuario tiene id 2
+                if (subscription.getUser().getId()== 2) {
+                    throw new Exception("Error intencional en setDateToday");
+                }
+
+            } catch (Exception e) {
+                // Capturar la excepción y no hacer nada (los cambios no se guardarán)
+                log.error("Se produjo una excepción en setDateToday, no se guardarán los cambios.", e);
+                throw new RuntimeException();
+            }
+        }
     }
-}
