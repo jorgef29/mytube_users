971e59c1a804a2833883d5fdfa689c8b1a665b2f
jorgef29 <jorgefigueredo2001@gmail.com>
2024-10-09 19:54:20 -0300

    cambios para funcionar con eureka

--- src/main/java/com/fiuni/mytube_users/MytubeUsersApplication.java
+++ src/main/java/com/fiuni/mytube_users/MytubeUsersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-
 @SpringBootApplication
 @EntityScan(basePackages = "com.fiuni.mytube.domain")
 @EnableCaching
+
 public class MytubeUsersApplication {
 
     public static void main(String[] args) {
