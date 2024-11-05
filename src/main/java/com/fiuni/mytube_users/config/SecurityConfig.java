0104715a4af198d92b67f634ee8f2de22f1c46ae
jorgef29 <jorgefigueredo2001@gmail.com>
2024-11-05 13:28:59 -0300

    add: ni idea

--- src/main/java/com/fiuni/mytube_users/config/SecurityConfig.java
+++ src/main/java/com/fiuni/mytube_users/config/SecurityConfig.java
@@ -8,27 +8,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-/*
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Permitir acceso sin restricciones a todas las rutas
+        // Permitir acceso sin restricciones a todas las rutas y deshabilitar CORS y CSRF
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()  // Permitir todas las solicitudes sin autenticaciÃ³n
+                        .anyRequest().permitAll()  // Permitir todas las solicitudes sin autenticación
                 )
+                .cors(AbstractHttpConfigurer::disable)  // Deshabilitar CORS
                 .csrf(AbstractHttpConfigurer::disable);  // Deshabilitar CSRF de manera segura
-
-        return http.build();
-    }*/
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())  // Deshabilitar CSRF (si es necesario)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/users/**").authenticated()  // Requiere autenticación para rutas de usuarios
-                        .anyRequest().permitAll()  // Permitir todas las demás solicitudes sin autenticación
-                );
 
         return http.build();
     }
