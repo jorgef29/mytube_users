88bd8620c055f482c2f3dc6a19ac80e24eb0dfe6
rodrigo-maidana <ro-maidana@hotmail.com>
2024-11-14 19:46:27 -0300

    remove cors and security cofig

--- src/main/java/com/fiuni/mytube_users/MytubeUsersApplication.java
+++ src/main/java/com/fiuni/mytube_users/MytubeUsersApplication.java
@@ -3,10 +3,12 @@ package com.fiuni.mytube_users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan(basePackages = "com.fiuni.mytube.domain")
-//@EnableCaching
+@EnableCaching
 
 public class MytubeUsersApplication {
 
