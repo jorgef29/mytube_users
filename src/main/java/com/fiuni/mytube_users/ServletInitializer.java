0e2b0d6dbaf9ef0ff5c3b5ae19e48ba05dbc719e
jorgef29 <jorgefigueredo2001@gmail.com>
2024-11-20 23:37:43 -0300

    add: ServletInitializer.java

---
+++ src/main/java/com/fiuni/mytube_users/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.fiuni.mytube_users;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MytubeUsersApplication.class);
+    }
+
+}
